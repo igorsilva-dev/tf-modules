3dddfe9f21cfb726df68bc077256628f6436d3c3
igorsilva-dev <dev@igorsilva.dev>
2026-02-04 20:24:58 +0100

    test: trying to fix the local file for kubeconfig

--- civo/kubernetes/outputs.tf
+++ civo/kubernetes/outputs.tf
@@ -2,3 +2,9 @@ output "cluster_name" {
   description = "Name of the created Civo Kubernetes cluster"
   value       = civo_kubernetes_cluster.cluster.name
 }
+
+output "kubeconfig" {
+  description = "Kubeconfig content for the created Civo Kubernetes cluster"
+  value       = civo_kubernetes_cluster.cluster.kubeconfig
+  sensitive   = true  
+}
